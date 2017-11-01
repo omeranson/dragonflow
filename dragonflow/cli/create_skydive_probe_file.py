4adb2b6ded4eb5977208063f47d4c42e364bcb71
Omer Anson <omer.anson@toganetworks.com>
2017-11-01 17:28:18 +0200

    [WIP]Optimize skydive output generation
    Change-Id: Id9b934ad014ecad4d0f74cd6e2974aa8b30c5fd1

--- dragonflow/cli/create_skydive_probe_file.py
+++ dragonflow/cli/create_skydive_probe_file.py
@@ -10,6 +10,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from jsonmodels import fields
 import uuid
 
 from oslo_serialization import jsonutils
@@ -23,7 +24,7 @@ from dragonflow.db.models import all  # noqa
 DF_SKYDIVE_NAMESPACE_UUID = uuid.UUID('8a527b24-f0f5-4c1f-8f3d-6de400aa0145')
 
 
-def output_edge(outf, nb_api, instance, field_name):
+def output_edge(edges, nb_api, instance, field_name):
     field_proxy = getattr(instance, field_name)
     field = nb_api.get(field_proxy)
     id_str = '{}->{}'.format(instance.id, field.id)
@@ -31,7 +32,6 @@ def output_edge(outf, nb_api, instance, field_name):
         'source': 'dragonflow',
         'source_type': type(instance).__name__,
         'dest_type': type(field).__name__,
-        "RelationType" : "ownership"
     }
     result = {
         'ID': str(uuid.uuid5(DF_SKYDIVE_NAMESPACE_UUID, id_str)),
@@ -40,49 +40,78 @@ def output_edge(outf, nb_api, instance, field_name):
         'Host': 'dragonflow',
         'Metadata': metadata
     }
-    outf(result)
+    edges.append(result)
 
-def output_edges(outf, nb_api, table_name, field_name):
-    model = mf.get_model(table_name)
-    instances = nb_api.get_all(model)
-    for instance in instances:
-        output_edge(outf, nb_api, instance, field_name)
+def output_table_node_edges(edges, nb_api, instance):
+    for key, field in type(instance).iterate_over_fields():
+        if isinstance(field, fields.ListField):
+            types = field.items_types
+            continue  # TODO(oanson) Not supported
+        else:
+            types = field.types
+        for field_type in types:
+            try:
+                model = field_type.get_proxied_model()
+                output_edge(edges, nb_api, instance, key)
+            except AttributeError:
+                pass  # ignore
+            break
 
-def output_table_node(outf, instance):
+def output_table_node(nodes, edges, nb_api, instance):
     metadata = {
         'ID': "DF-{}".format(instance.id),
-        'type': type(instance).__name__,
+        'Type': type(instance).__name__,
         'source': 'dragonflow',
         'data': instance.to_struct(),
-        'Name': getattr(instance, 'name') or instance.id
+        'Name': getattr(instance, 'name', None) or instance.id
     }
     result = {
         'Metadata': metadata,
         'ID': "DF-{}".format(instance.id),
         'Host': 'dragonflow'}
-    outf(result)
+    nodes.append(result)
+    output_table_node_edges(edges, nb_api, instance)
 
-def output_table(outf, nb_api, table_name):
+def output_table(nodes, edges, nb_api, table_name):
     model = mf.get_model(table_name)
     instances = nb_api.get_all(model)
     for instance in instances:
-        output_table_node(outf, instance)
+        output_table_node(nodes, edges, nb_api, instance)
 
-def main():
-    df_utils.config_parse()
-    nodes = []
+def output_tables():
     nb_api = api_nb.NbApi.get_instance(False)
-    output_table(nodes.append, nb_api, 'LogicalPort')
-    output_table(nodes.append, nb_api, 'LogicalSwitch')
+    nodes = []
     edges = []
-    output_edges(edges.append, nb_api, 'LogicalPort', 'lswitch')
+    for table_name in mf.iter_tables():
+        output_table(nodes, edges, nb_api, table_name)
     result = {
         'Nodes': nodes,
         'Edges': edges,
     }
+    return result
+
+def main():
+    df_utils.config_parse()
+    result = output_tables()
     with open('output.json', 'w') as f:
         jsonutils.dump(result, f)
     print('Done!')
+    #nodes = []
+    #nb_api = api_nb.NbApi.get_instance(False)
+    #output_table(nodes.append, nb_api, 'LogicalPort')
+    #output_table(nodes.append, nb_api, 'LogicalSwitch')
+    #edges = []
+    #output_edges(edges.append, nb_api, 'LogicalPort', 'lswitch')
+    #result = {
+    #    'Nodes': nodes,
+    #    'Edges': edges,
+    #}
+    #with open('output.json', 'w') as f:
+    #    jsonutils.dump(result, f)
+    #print('Done!')
 
 if __name__ == "__main__":
     main()
+
+
+# [stack@oanson-x4 skydive (master)]$ export GO_BINDATA_FLAGS=-debug
